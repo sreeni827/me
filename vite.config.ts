8a53805a85f16a19e19977829a84a90c487f56c9
Sreenu827 <sreenu.ramanaboina@gmail.com>
2025-03-04 09:49:01 -0600

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: '/Sreenivasulu_Ramanaboina_portfolio2/', // Set the base path for GitHub Pages
-  build: {
-    rollupOptions: {
-      input: './index.html', // Vite typically picks up the correct entry file automatically, so you can leave this out unless you have custom entry files
-    },
-  },
+  base: '/Sreenivasulu_Ramanaboina_portfolio2/', // Path for GitHub Pages
 });
