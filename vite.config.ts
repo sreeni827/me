1e49526376c45b5495af6b43c6de6fb5a56f231c
Sreenu827 <sreenu.ramanaboina@gmail.com>
2025-03-05 03:25:33 -0600

    dfgh

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sreenivasulu_Ramanaboina_portfolio2/', // Path for GitHub Pages
+  base: '/me/', // Path for GitHub Pages
 });
